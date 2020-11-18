c0deee46c8638f0abb96ba846f43d2024fc32ff2
Rossiel <rossielcarranza@gmail.com>
2020-11-17 23:04:25 -0300

    Add props validation in filter component
    - Connect redux to react with mapdispatchtoprops and mapstatetoprops

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchCategories } from '../actions';
@@ -8,9 +8,31 @@ const Filter = ({
 }) => {
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return (
     <p>Filter Component</p>
   );
 };
+
+Filter.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
+  fetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
+  fetchCategories: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = () => ({
+  fetchCategories,
+});
+
+const mapStateToProps = state => ({
+  categories: state.filter.categories,
+  fetching: state.fetch.fetching,
+  error: state.fetch.error,
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Filter);
