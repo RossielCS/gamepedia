bb18bbb7dc2e6e9acaba2fba32759b25e77951fc
Rossiel <rossielcarranza@gmail.com>
2020-11-26 20:52:19 -0300

    Add elements in footer component
    - Add icons for social media
    - Information about design author
    - Styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import github from '../assets/images/i-octocat-100.png';
+import linkedin from '../assets/images/i-linkedin-100.png';
+import mail from '../assets/images/i-letter-100.png';
 
 const Footer = () => (
-  <div className="Footer">Footer</div>
+  <div className="Footer">
+    <div className="footer-authors">
+      <p className="app-author">Built by Rossiel Carranza</p>
+      <p className="design-author">
+        <span>Design based in </span>
+        <a href="https://www.behance.net/gallery/11351281/NomNom" target="_blank" rel="noreferrer">
+          NomNom
+        </a>
+        <span> by </span>
+        <a href="https://www.behance.net/enfantroy" target="_blank" rel="noreferrer">
+          Marc-Antoine Roy
+        </a>
+      </p>
+    </div>
+    <div className="footer-icons">
+      <a href="https://github.com/RossielCS" target="_blank" rel="noreferrer">
+        <div className="icon-container">
+          <img src={github} alt="github" />
+          <span className="icon-title">Github</span>
+        </div>
+      </a>
+      <a href="https://www.linkedin.com/in/rossiel-carranza/" target="_blank" rel="noreferrer">
+        <div className="icon-container">
+          <img src={linkedin} alt="linkedin" />
+          <span className="icon-title">LinkedIn</span>
+        </div>
+      </a>
+      <a href="mailto:rossielcarranza@gmail.com">
+        <div className="icon-container">
+          <img src={mail} alt="email" />
+          <span className="icon-title">Email</span>
+        </div>
+      </a>
+    </div>
+  </div>
 );
 
 export default Footer;
