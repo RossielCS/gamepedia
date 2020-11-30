3a86d086f2a6e7f8b4c25ca2c04f3ff5ceed44e8
Rossiel <rossielcarranza@gmail.com>
2020-11-30 18:20:57 -0300

    Add unit test for redux reducers
    - Test for handling cases receive games list and receive game

--- src/tests/reducers.test.js
+++ src/tests/reducers.test.js
@@ -1,8 +1,34 @@
 import gamesReducer from '../reducers/games';
-import filtersReducer from '../reducers/filter';
+// import filtersReducer from '../reducers/filter';
 
 describe('games reducer', () => {
+  test('It should handle RECEIVE_GAMES_LIST', () => {
+    const action = {
+      type: 'RECEIVE_GAMES_LIST',
+      payload: [{}],
+    };
 
+    expect(gamesReducer(undefined, action)).toEqual({
+      items: [{}],
+      item: {},
+      fetching: false,
+      error: '',
+    });
+  });
+
+  test('It should handle RECEIVE_GAME', () => {
+    const action = {
+      type: 'RECEIVE_GAME',
+      payload: { id: 1234, name: 'Minecraft' },
+    };
+
+    expect(gamesReducer(undefined, action)).toEqual({
+      items: [],
+      item: { id: 1234, name: 'Minecraft' },
+      fetching: false,
+      error: '',
+    });
+  });
 });
 
 describe('filters reducer', () => {
